b267d949f6d2b52fb8e62bfa0b95ad2c7b0cbc43
mjaszc <jaszczaninmarceli@gmail.com>
2025-08-03 20:24:40 +0200

    Added support for reading files from stdin wihtout specified filename

--- src/pywc.py
+++ src/pywc.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import click
+import sys
 
 
 @click.command()
@@ -8,7 +9,7 @@ import click
 @click.option("-l", "count_lines", is_flag=True, help="Line count in the file")
 @click.option("-w", "count_words", is_flag=True, help="Words count in the file")
 @click.option("-m", "count_chars", is_flag=True, help="Character count in the file")
-@click.argument("filepath", type=click.Path(exists=True))
+@click.argument("filepath", required=False, type=click.Path(exists=True))
 def cli(
     count_bytes,
     count_lines,
@@ -16,26 +17,49 @@ def cli(
     count_chars,
     filepath,
 ):
-    if count_bytes:
+    if filepath == None:
+        content = sys.stdin.read()
+
+        byte_content = content.encode("utf-8")
+        byte_count = len(byte_content)
+        line_count = content.count("\n")
+        word_count = len(content.split())
+
+        if count_bytes:
+            click.echo(f"{byte_count}")
+        elif count_lines:
+            click.echo(f"{line_count}")
+        elif count_words:
+            click.echo(f"{word_count}")
+        elif count_chars:
+            char_count = len(byte_content.decode("utf-8"))
+            click.echo(f"{char_count}")
+        else:
+            click.echo(f"{byte_count} {word_count} {line_count}")
+    else:
         byte_count = os.path.getsize(Path(filepath))
-        click.echo(f"{byte_count} {filepath}")
-
-    if count_lines:
         line_count = sum(1 for _ in open(filepath))
-        click.echo(f"{line_count} {filepath}")
+        word_count = 0
 
-    if count_words:
-        with open(filepath) as f:
-            s = f.read()
-            word_list = s.split()
-            word_count = len(word_list)
-            click.echo(f"{word_count} {filepath}")
-
-    if count_chars:
-        with open(filepath, "rb") as f:
-            s = f.read()
-            char_count = len(s.decode("utf-8"))
-            click.echo(f"{char_count} {filepath}")
+        if count_bytes:
+            click.echo(f"{byte_count} {filepath}")
+        elif count_lines:
+            click.echo(f"{line_count} {filepath}")
+        elif count_words:
+            with open(filepath) as f:
+                s = f.read()
+                word_count = len(s.split())
+                click.echo(f"{word_count} {filepath}")
+        elif count_chars:
+            with open(filepath, "rb") as f:
+                s = f.read()
+                char_count = len(s.decode("utf-8"))
+                click.echo(f"{char_count} {filepath}")
+        else:
+            with open(filepath) as f:
+                s = f.read()
+                word_count = len(s.split())
+            click.echo(f"{byte_count} {word_count} {line_count} {filepath}")
 
 
 if __name__ == "__main__":
